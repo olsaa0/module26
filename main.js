22e76bd5f9b033f00fc7ea0fcb31d78a64ace16c
olsaa0 <olsaolsa30@gmail.com>
2024-10-03 16:16:08 +0200

    variabla etj

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("hello javascript!")
+
+var x = 10
+var teksti = "shkolla digjitale"
+var truth = true
+
+var a = 50 + 30
+var b = 30 - 15
+var c = 120/17
+var d = 30 % 7
+var e = 15 * 30
+var f = 3**2
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+a+=15
+console.log(a)
+
+a++;
+console.log(a)
+
+var numri1 = "20"
+var numri2 = 20
+
+console.log(numri1 == numri2)
+console.log(numri1 === numri2)
+
+console.log(!(numri1 === numri2))
